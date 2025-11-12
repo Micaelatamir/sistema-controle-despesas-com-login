f2e4c66839caf0ad890cfb5b7f57ac4ea4d588ca
Micaela Tamires <micaela.tamir@gmail.com>
2025-11-12 13:11:40 -0300

    Primeiro commit

---
+++ src/Conexao.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:postgresql://localhost:5432/sistema_controle_despesas_com_login";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "977310607";
+
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+            return null;
+        }
+    }
+}
